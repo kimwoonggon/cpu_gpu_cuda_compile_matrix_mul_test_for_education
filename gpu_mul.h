296cd6d93c8e33a76ee0549061265209ef11ab6a
statking <60002940+kimwoonggon@users.noreply.github.com>
2024-03-03 01:18:45 +0900

    Update gpu_mul.h

--- gpu_mul.h
+++ gpu_mul.h
@@ -1,3 +1,7 @@
 #include <cuda_runtime.h>
+#define TILE_WIDTH 32
 __global__ void matrixMultiplyKernel(float *a, float *b, float *c, int N);
 void gpuMatrixMultiply(float *a, float *b, float *c, int N);
+__global__ void matrixMultiplySharedKernel(float *a, float *b, float *c, int N);
+void gpuMatrixMultiplyShared(float *a, float *b, float *c, int N);
+
